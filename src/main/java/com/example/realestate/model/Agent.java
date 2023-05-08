49be903a8c31abdf76f1638d77e0c5665c558b73
Edgar Zambrana <edgarzambrana@Edgars-MacBook-Pro.local>
2023-05-08 16:04:53 -0400

    Write getters and setters for Agent class

--- src/main/java/com/example/realestate/model/Agent.java
+++ src/main/java/com/example/realestate/model/Agent.java
@@ -32,4 +32,44 @@ public class Agent {
         this.password = password;
         this.phoneNumber = phoneNumber;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
