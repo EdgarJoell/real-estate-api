bf04976851288cb75bb2094124d6d931302bc9a1
Lorena Rojas <lochan04@gmail.com>
2023-05-08 15:13:50 -0500

    added table columns

--- src/main/java/com/example/realestate/model/Property.java
+++ src/main/java/com/example/realestate/model/Property.java
@@ -1,8 +1,23 @@
 package com.example.realestate.model;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "properties")
-public class Properties {
+public class Property {
+
+    @Id
+    @Column
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long property_id;
+
+    @Column
+    private String address;
+
+    @Column
+    private double price;
+
+    @Column
+    private String size;
+
 }
