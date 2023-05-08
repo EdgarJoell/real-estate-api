e987cd8fd5cde01fe5bc1d67427b5348b9d04e1f
Maksym Zinchenko <maklaut98@gmail.com>
2023-05-08 14:49:47 -0500

    Added rest controller and mapping to AgentController

--- src/main/java/com/example/realestate/controller/AgentController.java
+++ src/main/java/com/example/realestate/controller/AgentController.java
@@ -1,4 +1,9 @@
 package com.example.realestate.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api")
 public class AgentController {
 }
