e34f6082db8f5b3ffffe29694155d51cbdad6f8a
Maksym Zinchenko <maklaut98@gmail.com>
2023-05-08 15:33:07 -0500

    Added getters and setters to sale model

--- src/main/java/com/example/realestate/model/Sale.java
+++ src/main/java/com/example/realestate/model/Sale.java
@@ -17,4 +17,36 @@ public class Sale {
     @Column
     private Date saleDate;
 
+    public Sale() {
+    }
+
+    public Sale(Long sale_id, double price, Date saleDate) {
+        this.sale_id = sale_id;
+        this.price = price;
+        this.saleDate = saleDate;
+    }
+
+    public Long getSale_id() {
+        return sale_id;
+    }
+
+    public void setSale_id(Long sale_id) {
+        this.sale_id = sale_id;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Date getSaleDate() {
+        return saleDate;
+    }
+
+    public void setSaleDate(Date saleDate) {
+        this.saleDate = saleDate;
+    }
 }
