27bfc60af13e0613aef226aee86a5eadd7be6bec
Edgar Zambrana <edgarzambrana@Edgars-MacBook-Pro.local>
2023-05-09 11:07:54 -0400

    Add PropertyController

---
+++ src/main/java/com/example/realestate/controller/PropertyController.java
@@ -0,0 +1,10 @@
+package com.example.realestate.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/properties/")
+public class PropertyController {
+
+}
