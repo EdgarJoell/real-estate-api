e11ec888b850dd16bad338f0a1bcce0bfe6b5ea5
Maksym Zinchenko <maklaut98@gmail.com>
2023-05-09 09:57:25 -0500

    Added configuration for cucumber test definition

--- src/test/java/definitions/SpringBootCucumberTestDefinitions.java
+++ src/test/java/definitions/SpringBootCucumberTestDefinitions.java
@@ -1,9 +1,21 @@
 package definitions;
 
+import com.example.realestate.RealEstateApplication;
 import io.cucumber.java.en.Given;
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
+@CucumberContextConfiguration
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = RealEstateApplication.class)
 public class SpringBootCucumberTestDefinitions {
+    private static final String BASE_URL = "http://localhost:";
+
+    @LocalServerPort
+    String port;
+
     @Given("A list of properties are available")
     public void aListOfPropertiesAreAvailable() {
+
     }
 }
