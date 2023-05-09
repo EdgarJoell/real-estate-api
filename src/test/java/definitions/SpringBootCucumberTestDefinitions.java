6d3619ed49f9479ea3b896d01b5c33f9ffe5e3c6
Maksym Zinchenko <maklaut98@gmail.com>
2023-05-09 10:01:39 -0500

    Added failing test for Get request

--- src/test/java/definitions/SpringBootCucumberTestDefinitions.java
+++ src/test/java/definitions/SpringBootCucumberTestDefinitions.java
@@ -3,8 +3,19 @@ package definitions;
 import com.example.realestate.RealEstateApplication;
 import io.cucumber.java.en.Given;
 import io.cucumber.spring.CucumberContextConfiguration;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.Assert;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.Map;
 
 @CucumberContextConfiguration
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = RealEstateApplication.class)
@@ -14,8 +25,20 @@ public class SpringBootCucumberTestDefinitions {
     @LocalServerPort
     String port;
 
+    private static Response response;
+
     @Given("A list of properties are available")
     public void aListOfPropertiesAreAvailable() {
-
+        try {
+            ResponseEntity<String> response = new RestTemplate().exchange(BASE_URL + port + "/api/properties/", HttpMethod.GET, null, String.class);
+            List<Map<String, String>> categories = JsonPath
+                    .from(String.valueOf(response
+                            .getBody()))
+                    .getList("$");
+            Assert.assertEquals(response.getStatusCode(), HttpStatus.OK);
+            Assert.assertTrue(categories.size() > 0);
+        } catch (HttpClientErrorException e) {
+            e.printStackTrace();
+        }
     }
 }
