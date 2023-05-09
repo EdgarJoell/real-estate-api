e8c7452000ae5f1d734fcdf816bb331d0c52bea5
Lorena Rojas <lochan04@gmail.com>
2023-05-09 15:33:02 -0500

    added doc strings

--- src/main/java/com/example/realestate/controller/PropertyController.java
+++ src/main/java/com/example/realestate/controller/PropertyController.java
@@ -1,7 +1,6 @@
 package com.example.realestate.controller;
 
 import com.example.realestate.model.Property;
-import com.example.realestate.repository.PropertyRepository;
 import com.example.realestate.service.PropertyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -30,22 +29,43 @@ public class PropertyController {
         return propertyService.getProperties();
     }
 
+    /**
+     * Calls on getProperty() from PropertyService
+     * @param propertyId property id we are searching for
+     * @return a property based on property id
+     */
     @GetMapping(path = "/properties/{propertyId}/")
     public Property getProperty(@PathVariable Long propertyId) {
         return propertyService.getProperty(propertyId);
     }
 
+    /**
+     * Calls on createProperty() from PropertyService and returns status 201 if successful
+     * @param propertyObject property we are adding
+     * @return a property object that we have added
+     */
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(path = "/properties/")
     public Optional<Property> createProperty(@RequestBody Property propertyObject) {
         return propertyService.createProperty(propertyObject);
     }
 
+    /**
+     * Calls on updateProperty() from PropertyService
+     * @param propertyId property id we are searching for
+     * @param propertyObject property object we are updating
+     * @return an updated property object
+     */
     @PutMapping(path = "/properties/{propertyId}/")
     public Optional<Property> updateProperty(@PathVariable Long propertyId, @RequestBody Property propertyObject) {
         return propertyService.updateProperty(propertyId,propertyObject);
     }
 
+    /**
+     * Calls on updateProperty() from PropertyService
+     * @param propertyId property id we are searching for
+     * @return a String stating whether the deletion was successful, throws an exception otherwise.
+     */
     @DeleteMapping(path = "/properties/{propertyId}")
     public String deleteProperty(@PathVariable Long propertyId) {
         return propertyService.deleteProperty(propertyId);
