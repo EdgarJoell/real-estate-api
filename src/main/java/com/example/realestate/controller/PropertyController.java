6b4659b778447981e30075650c5acb683fbecb54
Edgar Zambrana <edgarzambrana@Edgars-MacBook-Pro.local>
2023-05-09 11:11:08 -0400

    Add getProperties() method in Property

--- src/main/java/com/example/realestate/controller/PropertyController.java
+++ src/main/java/com/example/realestate/controller/PropertyController.java
@@ -1,10 +1,26 @@
 package com.example.realestate.controller;
 
+import com.example.realestate.model.Property;
+import com.example.realestate.repository.PropertyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping(path = "/properties/")
-public class PropertyController {
+import java.util.List;
 
+@RestController
+@RequestMapping(path = "/api/")
+public class PropertyController {
+    private PropertyRepository propertyRepository;
+
+    @Autowired
+    public void setPropertyRepository(PropertyRepository propertyRepository) {
+        this.propertyRepository = propertyRepository;
+    }
+
+    @GetMapping(path = "/properties/")
+    public List<Property> getProperties() {
+        return propertyRepository.findAll();
+    }
 }
