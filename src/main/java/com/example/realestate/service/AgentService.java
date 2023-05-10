b09fabb5caa9db941d8c415dc8f18ccde4fce0ea
Lorena Rojas <lochan04@gmail.com>
2023-05-10 11:44:32 -0500

    added constructor

--- src/main/java/com/example/realestate/service/AgentService.java
+++ src/main/java/com/example/realestate/service/AgentService.java
@@ -5,10 +5,14 @@ import com.example.realestate.model.Agent;
 import com.example.realestate.model.request.LoginRequest;
 import com.example.realestate.model.response.LoginResponse;
 import com.example.realestate.repository.AgentRepository;
+import com.example.realestate.security.JWTUtils;
+import com.example.realestate.security.MyAgentDetails;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -17,6 +21,25 @@ public class AgentService {
 
     private AgentRepository agentRepository;
     private AuthenticationManager authenticationManager;
+
+    private final PasswordEncoder passwordEncoder;
+
+    private JWTUtils jwtUtils;
+
+    private MyAgentDetails myAgentDetails;
+
+    @Autowired
+    public AgentService(AgentRepository agentRepository, @Lazy PasswordEncoder passwordEncoder,
+                       JWTUtils jwtUtils, @Lazy AuthenticationManager authenticationManager,
+                       @Lazy MyAgentDetails myAgentDetails) {
+        this.agentRepository = agentRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtUtils = jwtUtils;
+        this.authenticationManager = authenticationManager;
+        this.myAgentDetails = myAgentDetails;
+    }
+
+
 
     @Autowired
     public void setAgentRepository(AgentRepository agentRepository) {
