7659e45f49ea96f47258d1340dc2881eb6239f02
Lorena Rojas <lochan04@gmail.com>
2023-05-10 09:33:17 -0500

    added existsByEmailAddress and findAgentByEmailAddress

--- src/main/java/com/example/realestate/repository/AgentRepository.java
+++ src/main/java/com/example/realestate/repository/AgentRepository.java
@@ -1,4 +1,13 @@
 package com.example.realestate.repository;
 
-public class AgentRepository {
+import com.example.realestate.model.Agent;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AgentRepository extends JpaRepository<Agent, Long> {
+
+    //Login
+    boolean existsByEmailAddress(String email);
+
+    //Register
+    Agent findAgentByEmailAddress(String email);
 }
