183bfaf291c5e3f94cbd1411e2c3f6f81c4d4f8e
Lorena Rojas <lochan04@gmail.com>
2023-05-10 11:21:41 -0500

    added generateJwtToken

--- src/main/java/com/example/realestate/security/JWTUtils.java
+++ src/main/java/com/example/realestate/security/JWTUtils.java
@@ -1,8 +1,33 @@
 package com.example.realestate.security;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.logging.Logger;
 
 @Service
 public class JWTUtils {
+
+    Logger logger = Logger.getLogger(JWTUtils.class.getName());
+
+    @Value("${jwt-secret}")
+    private String jwtSecret;
+
+    @Value("${jwt-expiration-ms}")
+    private int jwtExpirationMs;
+
+    public String generateJwtToken(MyAgentDetails myAgentDetails) {
+        return Jwts.builder()
+                .setSubject((myAgentDetails.getUsername()))
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .signWith(SignatureAlgorithm.HS256, jwtSecret)
+                .compact();
+    }
+
+
 
 }
