826d8fa1b948b25c8e6972fa305540874c53782c
Edgar Zambrana <edgarzambrana@Edgars-MacBook-Pro.local>
2023-05-10 11:02:59 -0400

    Create LoginRequest class

--- src/main/java/com/example/realestate/controller/AgentController.java
+++ src/main/java/com/example/realestate/controller/AgentController.java
@@ -3,13 +3,14 @@ package com.example.realestate.controller;
 import com.example.realestate.model.Agent;
 import com.example.realestate.service.AgentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "/api")
+@RequestMapping(path = "/auth/")
 public class AgentController {
     private AgentService agentService;
 
